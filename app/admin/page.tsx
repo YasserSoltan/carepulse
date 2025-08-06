715f9f2b41eb2a07a1b7fb2e0db8312dc0797caa
YasserSoltan <yassersoltan64@gmail.com>
2025-08-06 04:14:32 +0300

    fix import { columns } from "@/components/table/columns"

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ import StatCard from "@/components/StatCard";
 import { getRecentAppointments } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
-import { columns } from "@/components/table/Columns";
+import { columns } from "@/components/table/columns";
 
 const Admin = async () => {
   const appointments = await getRecentAppointments();
